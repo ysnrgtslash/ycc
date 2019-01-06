b820014006d7949f7736bed47bdad5202cd2984c
ysnrgtslash <10042604+ysnrgtslash@users.noreply.github.com>
2019-01-07 04:35:21 +0900

    plus minus

--- ycc.c
+++ ycc.c
@@ -2,15 +2,33 @@
 #include <stdlib.h>
 
 int main(int argc, char **argv) {
+
   if (argc != 2) {
     fprintf(stderr, "Args are no corret\n");
     return 1;
   }
+  char *p = argv[1];
 
   printf(".intel_syntax noprefix\n");
   printf(".global main\n");
   printf("main:\n");
-  printf("  mov rax, %d\n", atoi(argv[1]));
+  printf("  mov rax, %d\n", strtol(p, &p, 10));
+  while(*p) {
+  if (*p == '+') { 
+    p++;
+    printf("  add rax, %ld\n", strtol(p, &p, 10));
+    continue;
+   }
+  if (*p == '-') {
+    p++;
+    printf("  sub rax, %ld\n", strtol(p, &p, 10));
+    continue;
+   }
+ fprintf(stderr,"Unexpected str: '%c'\n", *p);
+    return 1;
+  }
+
+
   printf("  ret\n");
   return 0;
 }
